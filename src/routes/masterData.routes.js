414ee18a0cebe148262dbe782580fba64cc2a158
rifkyhilman <rifkyhilman265@gmail.com>
2025-05-11 21:35:20 +0700

    :sparkles: Add route to rest API CRUD products
    Add route to rest API CRUD products

--- src/routes/masterData.routes.js
+++ src/routes/masterData.routes.js
@@ -8,10 +8,25 @@ const {
     getCategoryById 
 } = require('../controllers/category.controller');
 
+const { 
+    createProduct, 
+    getAllProducts, 
+    deleteProduct, 
+    editProduct, 
+    getProductById 
+} = require('../controllers/product.controller');
+
 router.post('/category', createCategory);
 router.get('/category', getAllCategories);
 router.get('/category/:id', getCategoryById);
 router.put('/category/:id', editCategory);
 router.delete('/category/:id', deleteCategory);
+
+router.post('/product', createProduct);
+router.get('/product', getAllProducts);
+router.get('/product/:id', getProductById);
+router.put('/product/:id', editProduct);
+router.delete('/product/:id', deleteProduct);
+
 
 module.exports = router;
